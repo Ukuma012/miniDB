11ceab8cbfe57b048b4dfd3afe23c6823633d02d
Ukuma012 <Ukuma012@gmail.com>
2023-08-29 10:22:43 +0900

    prompt

--- minidb.c
+++ minidb.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  printf("%s\n", "Hello World from minidb!");
+#define PROMPT true
+
+int main(int argc, char *argv[])
+{
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+
+  while (PROMPT)
+  {
+    printf("%s", "minidb>> ");
+    if ((nread = getline(&line, &len, stdin)) > 0)
+    {
+      // 末尾の改行を終端に変更
+      char *p;
+      p = strchr(line, '\n');
+      if (p != NULL)
+      {
+        *p = '\0';
+      }
+
+      if (strcmp(line, "exit") == 0)
+      {
+        exit(0);
+      }
+    }
+  }
   return 0;
 }
