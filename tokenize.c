8a0b74ce622e474bf508276be5c1023acd68a3cf
Ukuma012 <Ukuma012@gmail.com>
2023-08-30 11:00:45 +0900

    tokenize

---
+++ tokenize.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "tokenize.h"
+
+int tokenize() {
+    printf("%s\n", "hello world");
+    return 0;
+}
---
+++ tokenize.h
@@ -0,0 +1,6 @@
+#ifndef _tokenize.h
+#define _tokenize.h
+
+int tokenize() {}
+
+#endif
