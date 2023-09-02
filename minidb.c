4b95e2293701c5970dcea34d1aa8fdc7fc511ba7
Ukuma012 <Ukuma012@gmail.com>
2023-09-02 16:39:55 +0900

    modified

--- minidb.c
+++ minidb.c
@@ -7,67 +7,67 @@
 #include "parser.h"
 
 typedef struct {
-		char *buffer;
-		size_t buffer_length;
-		ssize_t input_length;
+	char *buffer;
+	size_t buffer_length;
+	ssize_t input_length;
 } InputBuffer;
 
 InputBuffer *new_input_buffer() {
-		InputBuffer *input_buffer = calloc(1, sizeof(InputBuffer));
-		return input_buffer;
+	InputBuffer *input_buffer = calloc(1, sizeof(InputBuffer));
+	return input_buffer;
 }
 
 void print_prompt() {
-		printf("%s", "minidb>> ");
+	printf("%s", "minidb>> ");
 }
 
 void read_input(InputBuffer *input_buffer) {
-		ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-		if(bytes_read <= 0) {
-				fprintf(stderr, "error reading input\n");
-				exit(1);
-		}
+	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+	if(bytes_read <= 0) {
+		fprintf(stderr, "error reading input\n");
+		exit(1);
+	}
 
-		// 末尾の改行を無視
-		input_buffer->input_length = bytes_read - 1;
-		input_buffer->buffer[bytes_read - 1] = 0;
+	// 末尾の改行を無視
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
 }
 
 void close_input_bufer(InputBuffer *input_buffer) {
-		free(input_buffer->buffer);
-		free(input_buffer);
+	free(input_buffer->buffer);
+	free(input_buffer);
 }
 
 int main(int argc, char *argv[])
 {
-		Token* token;
-		Statement statement;
-		InputBuffer *input_buffer = new_input_buffer();
-		while(true) {
-				print_prompt();
-				read_input(input_buffer);
+	Token* token;
+	Statement statement;
+	InputBuffer *input_buffer = new_input_buffer();
+	while(true) {
+		print_prompt();
+		read_input(input_buffer);
 
-				if(strcmp(input_buffer->buffer, "exit") == 0) {
-						close_input_bufer(input_buffer);
-						exit(0);
-				} else {
-						token = tokenize(input_buffer->buffer);
-						switch(parse_statement_type(token, &statement)) {
-								case(PREPARE_SECCESS):
-										break;
-								case(PREPARE_SYNTAX_ERROR):
-										printf("%s\n", "Syntax Error");
-										continue;
-								case(PREPARE_UNRECOGNIZED_STATEMENT):
-										printf("Unrecognized keyword at start of '%s'.\n", token->value);
-										continue;
-						}
+		if(strcmp(input_buffer->buffer, "exit") == 0) {
+			close_input_bufer(input_buffer);
+			exit(0);
+		} else {
+			token = tokenize(input_buffer->buffer);
+			switch(parse_statement_type(token, &statement)) {
+				case(PREPARE_SECCESS):
+					break;
+				case(PREPARE_SYNTAX_ERROR):
+					printf("%s\n", "Syntax Error. Could not parse statement.");
+					continue;
+				case(PREPARE_UNRECOGNIZED_STATEMENT):
+					printf("Unrecognized keyword at start of '%s'.\n", token->value);
+					continue;
+			}
 
-						execute_statement(&statement);
-						printf("%s\n", "Executed");
+			execute_statement(&statement);
+			printf("%s\n", "Executed");
 
-						free_token(token);
-				}
+			free_token(token);
 		}
-		return 0;
+	}
+	return 0;
 }
--- parser.c
+++ parser.c
@@ -4,58 +4,59 @@
 #include "parser.h"
 
 ParseResult parse_statement_type(Token* tokens, Statement* statement) {
-		if(tokens->kind == TK_UNKNOWN) {
-			return PREPARE_UNRECOGNIZED_STATEMENT;
+	if(tokens->kind == TK_UNKNOWN) {
+		return PREPARE_UNRECOGNIZED_STATEMENT;
+	}
+
+	if(tokens->kind == TK_KEYWORD && strcmp(tokens->value, "SELECT") == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SECCESS;
+	}
+
+	// INSERT 
+	// INSERT 1 Ukuma ukuma@gmail.com
+	if(tokens->kind == TK_KEYWORD && strcmp(tokens->value, "INSERT") == 0) {
+		statement->type = STATEMENT_INSERT;
+		// @todo function!
+
+		tokens = tokens->next;
+		if(tokens->kind == TK_NUM) {
+			uint32_t num;
+			num = strtol(tokens->value, NULL, 10);
+			statement->row_to_insert.id = num;
+		} else {
+			return PREPARE_SYNTAX_ERROR;
 		}
 
-    if(tokens->kind == TK_KEYWORD && strcmp(tokens->value, "SELECT") == 0) {
-        statement->type = STATEMENT_SELECT;
-        return PREPARE_SECCESS;
-    }
+		tokens = tokens->next;
+		if(tokens->kind == TK_IDENTIFIER) {
+			statement->row_to_insert.username = strdup(tokens->value);
+		} else {
+			return PREPARE_SYNTAX_ERROR;
+		}
 
-		// INSERT 
-		// INSERT 1 Ukuma ukuma@gmail.com
-    if(tokens->kind == TK_KEYWORD && strcmp(tokens->value, "INSERT") == 0) {
-        statement->type = STATEMENT_INSERT;
+		tokens = tokens->next;
+		if(tokens->kind == TK_IDENTIFIER) {
+			statement->row_to_insert.email = strdup(tokens->value);
+		} else {
+			return PREPARE_SYNTAX_ERROR;
+		}
 
-				tokens = tokens->next;
-				if(tokens->kind == TK_NUM) {
-					uint32_t num;
-					num = strtol(tokens->value, NULL, 10);
-					statement->row_to_insert.id = num;
-				} else {
-					return PREPARE_SYNTAX_ERROR;
-				}
+		return PREPARE_SECCESS;
+	}
 
-				tokens = tokens->next;
-				if(tokens->kind == TK_IDENTIFIER) {
-					statement->row_to_insert.username = strdup(tokens->value);
-				} else {
-					return PREPARE_SYNTAX_ERROR;
-				}
+	return PREPARE_UNRECOGNIZED_STATEMENT;
 
-				tokens = tokens->next;
-				if(tokens->kind == TK_IDENTIFIER) {
-					statement->row_to_insert.email = strdup(tokens->value);
-				} else {
-					return PREPARE_SYNTAX_ERROR;
-				}
-
-        return PREPARE_SECCESS;
-    }
-
-    return PREPARE_UNRECOGNIZED_STATEMENT;
-    
 }
 
 void execute_statement(Statement* statment) {
-    switch (statment->type) {
-        case (STATEMENT_INSERT):
-            printf("%s", "This is where we would do an insert.\n");
-            break;
+	switch (statment->type) {
+		case (STATEMENT_INSERT):
+			printf("%s", "This is where we would do an insert.\n");
+			break;
 
-        case (STATEMENT_SELECT):
-            printf("%s", "This is where we would do a select.\n");
-            break;
-    }
+		case (STATEMENT_SELECT):
+			printf("%s", "This is where we would do a select.\n");
+			break;
+	}
 }
